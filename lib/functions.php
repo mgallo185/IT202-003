792f7bb32c8ee6e9c7120ba6b3d27987db9c3e74
mgallo185 <89924299+mgallo185@users.noreply.github.com>
2021-10-11 09:37:39 -0400

    Checkpoints

---
+++ lib/functions.php
@@ -0,0 +1,29 @@
+<?php
+require_once(__DIR__ . "/db.php");
+
+function se($v, $k = null, $default = "", $isEcho = true)
+{
+    if (is_array($v) && isset($k) && isset($v[$k])) {
+        $returnValue = $v[$k];
+    } else if (is_object($v) && isset($k) && isset($v->$k)) {
+        $returnValue = $v->$k;
+    } else {
+        $returnValue = $v;
+        //added 07-05-2021 to fix case where $k of $v isn't set
+        //this is to kep htmlspecialchars happy
+        if (is_array($returnValue) || is_object($returnValue)) {
+            $returnValue = $default;
+        }
+    }
+    if (!isset($returnValue)) {
+        $returnValue = $default;
+    }
+    if ($isEcho) {
+        //https://www.php.net/manual/en/function.htmlspecialchars.php
+        echo htmlspecialchars($returnValue, ENT_QUOTES);
+    } else {
+        //https://www.php.net/manual/en/function.htmlspecialchars.php
+        return htmlspecialchars($returnValue, ENT_QUOTES);
+    }
+}
+?>
---
+++ public_html/Project/register.php
@@ -0,0 +1,35 @@
+<form onsubmit="return validate(this)" method="POST">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" required />
+    </div>
+    <div>
+        <label for="pw">Password</label>
+        <input type="password" id="pw" name="password" required minlength="8" />
+    </div>
+    <div>
+        <label for="confirm">Confirm</label>
+        <input type="password" name="confirm" required minlength="8" />
+    </div>
+    <input type="submit" value="Register" />
+</form>
+<script>
+    function validate(form) {
+        //TODO 1: implement JavaScript validation
+        //ensure it returns false for an error and true for success
+
+        return true;
+    }
+</script>
+<?php
+ //TODO 2: add PHP Code
+ if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])){
+     $email = $_POST["email"];
+
+     $password = $_POST["password"];
+
+     $email = $_POST["confirm"];
+     
+     //TODO 3: validate/use
+ }
+?>
