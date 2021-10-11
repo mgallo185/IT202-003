4edb104e8488ae742f85c01d7aeb35eb72fea157
mgallo185 <89924299+mgallo185@users.noreply.github.com>
2021-10-11 11:13:29 -0400

    userProfile changes

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -7,6 +7,10 @@ require(__DIR__ . "/../../partials/nav.php");
         <input type="email" name="email" required />
     </div>
     <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" required maxlength="30" />
+    </div>
+    <div>
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
@@ -30,6 +34,7 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
     $email = se($_POST, "email", "", false);
     $password = se($_POST, "password", "", false);
     $confirm = se($_POST, "confirm", "", false);
+    $username = se($_POST, "username", "", false);
     //TODO 3
 
 
@@ -45,6 +50,10 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
     //if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     if (!is_valid_email($email)) {
         flash("Invalid email");
+        $hasError = true;
+    }
+    if (!preg_match('/^[a-z0-9_-]{3,16}$/', $username)) {
+        flash("Username must only be alphanumeric and can only contain - or _");
         $hasError = true;
     }
     if (empty($password)) {
@@ -66,12 +75,12 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
     if ($hasError) {
         //flash("<pre>" . var_export($errors, true) . "</pre>");
     } else {
-        flash("Welcome, $email");//will show on home.php
+        flash("Welcome, $email"); //will show on home.php
         $hash = password_hash($password, PASSWORD_BCRYPT);
         $db = getDB();
-        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+        $stmt = $db->prepare("INSERT INTO Users (email, password, username) VALUES(:email, :password, :username)");
         try {
-            $stmt->execute([":email" => $email, ":password" => $hash]);
+            $stmt->execute([":email" => $email, ":password" => $hash, ":username" => $username]);
             flash("You've registered, yay...");
         } catch (Exception $e) {
             flash("There was a problem registering");
