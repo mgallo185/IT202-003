2ca95e1a3dbee5567e5f1a972e6dbfd32c981b7f
mgallo185 <89924299+mgallo185@users.noreply.github.com>
2021-10-25 10:52:13 -0400

    Class Housekeeping

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -3,8 +3,8 @@ require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-if (is_logged_in()) {
-    echo "Welcome home, " . get_user_email();
+if (is_logged_in(true)) {
+    echo "Welcome home, " . get_username();
     //comment this out if you don't want to see the session variables
     echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 } else {
