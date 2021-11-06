132b1a7c93bf99a7e7492602a36a117f4baf93cf
mgallo185 <89924299+mgallo185@users.noreply.github.com>
2021-11-06 17:14:58 -0400

    nav and login part 2

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,5 +1,6 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php"); ?>
+require(__DIR__ . "/../../partials/nav.php");
+?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
@@ -22,46 +23,34 @@ require(__DIR__ . "/../../partials/nav.php"); ?>
 <?php
 //TODO 2: add PHP Code
 if (isset($_POST["email"]) && isset($_POST["password"])) {
-    //get the email key from $_POST, default to "" if not set, and return the value
     $email = se($_POST, "email", "", false);
-    //same as above but for password
     $password = se($_POST, "password", "", false);
-    //TODO 3: validate/use
-    //$errors = [];
-    $hasErrors = false;
+
+    //TODO 3
+    $hasError = false;
     if (empty($email)) {
-        //array_push($errors, "Email must be set");
-        flash("Email must be set", "warning");
-        $hasErrors = true;
+        echo "Email must not be empty";
+        $hasError = true;
     }
     //sanitize
-    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    $email = sanitize_email($email);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
     //validate
-    //if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    if (!is_valid_email($email)) {
-        //array_push($errors, "Invalid email address");
-        flash("Invalid email address", "warning");
-        $hasErrors = true;
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email address";
+        $hasError = true;
     }
     if (empty($password)) {
-        //array_push($errors, "Password must be set");
-        flash("Password must be set");
-        $hasErrors = true;
+        echo "password must not be empty";
+        $hasError = true;
     }
     if (strlen($password) < 8) {
-        //array_push($errors, "Password must be 8 or more characters");
-        flash("Password must be at least 8 characters", "warning");
-        $hasErrors = true;
+        echo "Password too short";
+        $hasError = true;
     }
-    if ($hasErrors) {
-        //Nothing to output here, flash will do it
-        //can likely flip the if condition
-        //echo "<pre>" . var_export($errors, true) . "</pre>";
-    } else {
+    if (!$hasError) {
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, password from Users where email = :email");
+        $stmt = $db->prepare("SELECT email, password from Users where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
@@ -70,25 +59,19 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     $hash = $user["password"];
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
-                        ///echo "Weclome $email";
+                        echo "Weclome $email";
                         $_SESSION["user"] = $user;
                         die(header("Location: home.php"));
                     } else {
-                        //echo "Invalid password";
-                        flash("Invalid password", "danger");
+                        echo "Invalid password";
                     }
                 } else {
-                    //echo "Invalid email";
-                    flash("Email not found", "danger");
+                    echo "Email not found";
                 }
             }
         } catch (Exception $e) {
-            //echo "<pre>" . var_export($e, true) . "</pre>";
-            flash(var_export($e, true));
+            echo "<pre>" . var_export($e, true) . "</pre>";
         }
     }
 }
-?>
-<?php
-require(__DIR__ . "/../../partials/flash.php");
 ?>
