a438792724cc93b235022811715db61673f524c9
mgallo185 <89924299+mgallo185@users.noreply.github.com>
2021-11-09 11:07:39 -0500

    fixes

--- lib/functions.php
+++ lib/functions.php
@@ -103,10 +103,6 @@ function reset_session()
 {
     session_unset();
     session_destroy();
-    session_start();
-
- 
-
 }
 function users_check_duplicate($errorInfo)
 {
@@ -123,7 +119,6 @@ function users_check_duplicate($errorInfo)
         //TODO come up with a nice error message
         flash("<pre>" . var_export($errorInfo, true) . "</pre>");
     }
-
 }
 function get_url($dest)
 {
@@ -134,5 +129,4 @@ function get_url($dest)
     }
     //handle relative path
     return $BASE_PATH . $dest;
-
 }
