1b063bb7c5f257cf4c9561691a5ff869342196aa
mgallo185 <89924299+mgallo185@users.noreply.github.com>
2021-11-29 08:18:27 -0500

    profile.php

--- public_html/Project/profile.php
+++ public_html/Project/profile.php
@@ -87,6 +87,26 @@ $username = get_username();
 ?>
 <div class="container-fluid">
     <h1>Profile</h1>
+    <div>
+        Best Score: <?php echo get_best_score($user_id); ?>
+    </div>
+    <div>
+        <?php $scores = get_latest_scores($user_id); ?>
+        <h3>Score History</h3>
+        <table class="table text-light">
+            <thead>
+                <th>Score</th>
+                <th>Time</th>
+            </thead>
+            <tbody>
+                <?php foreach ($scores as $score) : ?>
+                    <tr>
+                        <td><?php se($score, "score", 0); ?></td>
+                        <td><?php se($score, "created", "-"); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     <form method="POST" onsubmit="return validate(this);">
         <div class="mb-3">
             <label class="form-label" for="email">Email</label>
