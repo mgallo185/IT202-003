c5bcd89ca989a5ec7a636277a4d0bd9f43ac4628
mgallo185 <89924299+mgallo185@users.noreply.github.com>
2021-12-19 17:43:25 -0500

    fixes

--- public_html/Project/create_competitions.php
+++ public_html/Project/create_competitions.php
@@ -2,7 +2,7 @@
 <?php
 require_once(__DIR__ . "/../../partials/nav.php");
 is_logged_in(true);
-
+$payout_options = [];
 $db = getDB();
 $stmt = $db->prepare("SELECT id, CONCAT(first_place,'% - ', second_place, '% - ', third_place, '%') as place FROM Competitions");
 try {
