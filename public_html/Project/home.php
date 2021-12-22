bab524ce71b8650573b695852ff208e88e0c63ac
mgallo185 <89924299+mgallo185@users.noreply.github.com>
2021-12-21 21:21:34 -0500

    Update home.php

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -2,14 +2,13 @@
 require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
-
+<h3><b><a class="nav-link" href="game.php">Play Game</a><b></h3>
 <form method= "POST" action = "home.php">
 
 <input type = "submit" name="time" class = "navbar-nav me-auto mb-2 mb-lg-0" value= "day" />
 <input type = "submit" name="time" class = "navbar-nav me-auto mb-2 mb-lg-0" value= "week" />
 <input type = "submit" name="time" class = "navbar-nav me-auto mb-2 mb-lg-0" value= "month" />
 <input type = "submit" name="time" class = "navbar-nav me-auto mb-2 mb-lg-0" value= "lifetime" />
-<input type="submit" name="game" class="mt-3 btn btn-success" value="Play Game" action = "game.php" />
 
 </form> 
 
